fa395ee0f8b0d7f4ae888356714a8a3e8eb39907
randonie <test@sa.com>
2018-05-17 12:53:47 -0700

    Initial Check-in.

---
+++ Project/Advanced/NewHeader.h
@@ -0,0 +1,8 @@
+#define glossary 1000
+#define links 1001
+#define snippetandmasterpage 1002
+#define Topic 1003
+#define another_mixed_up 1004
+#define anothersubindex 1005
+#define subindexVariable 1006
+#define test 1007
